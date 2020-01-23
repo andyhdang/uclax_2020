87bfc77fa947eae518520442d2a4db1510453316
Andy Dang <andyhd0614@gmail.com>
2020-01-22 21:38:49 -0800

    updated hamburger menu toggle

--- src/App/Nav/Nav.jsx
+++ src/App/Nav/Nav.jsx
@@ -1,21 +1,56 @@
-import React from 'react'
+//useState is part of React App
+import React, { useState } from 'react'
 import './Nav.scss'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+
 const Nav = () => {
+
+    //showMenu and updateShowMenu are made up names
+    const [showMenu, updateShowMenu] = useState (true)
+
+    const handleHamburgerClick = () => {
+        console.log ('You hit the hamburger')
+
+        let newShowMenu;
+
+        if (showMenu === true) {
+            newShowMenu = false;
+        } else {
+            newShowMenu = true;
+        }
+
+        updateShowMenu(newShowMenu)
+        
+
+    }
     return (
 
         //JSX
         <nav className={'Nav'}>
             
-            <div className="hamburger">Hamburger</div>
+            <div className="hamburger" onClick = {handleHamburgerClick}> 
 
-            <div className="links">
-            
-                <a href="#">Welcome</a> 
-                <a href="#">Services</a>
-                <a href="#">Contact</a>
-
+                <FontAwesomeIcon icon ={faBars}></FontAwesomeIcon>
+                
             </div>
+                
+            {
+            
+                showMenu &&
+
+                <div className="links">
+                
+                    <a href="#">Welcome</a> 
+                    <a href="#">Services</a>
+                    <a href="#">Contact</a>
+
+                </div>
+
+            }
+
 
         </nav>
     );
