e8161643861383de9b589726e9dcc09ba88668df
Andy Dang <andyhd0614@gmail.com>
2020-01-29 19:22:52 -0800

    Refactored Navigation Hamburger Icon

--- src/App/Nav/Nav.jsx
+++ src/App/Nav/Nav.jsx
@@ -8,67 +8,46 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Nav = () => {
 
-    //showMenu and updateShowMenu are made up names
-    //state variables 
-    const [showMenu, updateShowMenu] = useState (true);
-    const [isSmall, updateIsSmall] = useState(true);
+    const [showMenu, updateShowMenu] = useState(false);
 
-    //Functions / Methods
-    //Hamburger Click
+    //Click Event
     const handleHamburgerClick = () => {
-        console.log ('You hit the hamburger');
 
-        let newShowMenu;
-
-        if (showMenu === true) {
-            newShowMenu = false;
+        if (showMenu === false) {
+            updateShowMenu(true);
         } else {
-            newShowMenu = true;
+            updateShowMenu(false);
         }
+    } 
 
-        updateShowMenu(newShowMenu);
-
-    }
-    
-    // Checking for Small view or Medium/Large
-    const handleIsSmall = () => {
-        console.log('How Wide Window: ', window.innerWidth, isSmall);
+    //Window Resize Event
+    const handleWindowResize = () => {
 
         if (window.innerWidth > 599) {
-            updateIsSmall(false); //not small
-            updateShowMenu(true); //show menu
+            updateShowMenu(true);
         } else {
-            updateIsSmall(true); //is small
-            updateShowMenu(false); //hide menu
+            updateShowMenu(false);
         }
-
     }
 
-    //useEffect so it doesnt keep reassigning the listener
-    useEffect(()=> {
-        window.addEventListener('resize', handleIsSmall);
-        handleIsSmall(); //fires off when page first loads
-    }, []);
-   
+    //When component mounts
+    useEffect (() => {
+        window.addEventListener('resize', handleWindowResize);
+        handleWindowResize();
+    }, [])
 
     return (
 
         //JSX
         <nav className='Nav'>
 
-            {
-                isSmall &&
-                // <button onClick={ handleIsSmall }>Clickme</button>
-                <div className="hamburger" onClick = {handleHamburgerClick}> 
-                    <FontAwesomeIcon icon ={faBars}></FontAwesomeIcon>
+                <div className="hamburger" onClick= { handleHamburgerClick }>
+                     <FontAwesomeIcon icon ={faBars}></FontAwesomeIcon>
                 </div>
 
-            }
+                {
 
-            {
-             //"if showMenu is true" show below
-                showMenu &&
-
+                showMenu && //if showMeny is true, show this menu
                 <div className="links">
                 
                     <a href="#">Welcome</a> 
@@ -77,8 +56,7 @@ const Nav = () => {
 
                 </div>
 
-            }
-
+                }
 
         </nav>
     );
